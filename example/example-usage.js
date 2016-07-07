68ed1600b2f658012d7b49f510b5a1038b160edf
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-07 17:28:29 +0900

    Version incremented to 1.0.1

--- example/example-usage.js
+++ example/example-usage.js
@@ -1,3 +1,26 @@
 'use strict'
 
 const aslock = require('aslock')
+const co = require('co')
+
+co(function * () {
+  // Acquire a lock and do action
+  yield aslock.acquire('my-exclusive-work-01', () => co(function * () {
+    // Some async actions
+    /* ... */
+  }))
+
+  // Non block without yield
+  aslock.acquire('my-exclusive-work-01', () => co(function * () {
+    return new Promise((resolve) => setTimeout(resolve, 300))
+  }))
+
+  try {
+    aslock.acquire('my-exclusive-work-01', () => co(function * () {
+      /* .. */
+    })) // -> This throws error since the lock taken by other.
+  } catch (err) {
+    console.error('Failed')
+  }
+}).catch((err) => console.error(err))
+
